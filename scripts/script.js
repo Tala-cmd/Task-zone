fdf3dbfb01596080e0ba636ea31e7344c13af5f2
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-09-22 17:16:08 +0300

    Edit button -1

--- scripts/script.js
+++ scripts/script.js
@@ -11,33 +11,57 @@ const todoList = [{
   function renderTodoList() {
     let todoListHTML = '';
   
-    todoList.forEach((todoObject, index) => {
-      const { name, dueTime } = todoObject;
-      const html = `
-      <div class="todo-container">
-        <div class="todo-name">${name}</div>
-        <div class="todo-time">${dueTime}</div>
-        <button class="edit-todo-button"> <i class="ri-edit-fill"> </i></button>
-        <button class="delete-todo-button js-delete-todo-button"> <i class="ri-close-circle-fill"> </i></button> 
-      </div>
-      `;
-      todoListHTML += html;
+      todoList.forEach((todoObject, index) => {
+        const { name, dueTime } = todoObject;
+        const html = `
+        <div class="todo-container">
+          <div class="todo-name">${name}</div>
+          <div class="todo-time">${dueTime}</div>
+          <button class="edit-todo-button js-edit-todo-button"> <i class="ri-edit-fill"> </i></button>
+          <button class="delete-todo-button js-delete-todo-button"> <i class="ri-delete-bin-line"></i> </i></button> 
+        </div>
+        `;
+        todoListHTML += html;
     });
   
-    document.querySelector('.js-todo-list')
-      .innerHTML = todoListHTML;
-  
-    document.querySelectorAll('.js-delete-todo-button')
-      .forEach((deleteButton, index) => {
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    
+    document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
         deleteButton.addEventListener('click', () => {
           todoList.splice(index, 1);
           renderTodoList();
         });
       });
+
+      document.querySelectorAll('.js-edit-todo-button').forEach((editButton, index) => {
+        editButton.addEventListener('click',() => {
+        
+          const todoContainer = editButton.closest('.todo-container');
+          const taskText = todoContainer.querySelector('.todo-name');
+
+          const editInput = document.createElement('input');
+          editInput.type = 'text';
+          editInput.value = taskText.textContent;
+        
+          // Replace the task text with the input element
+          todoContainer.replaceChild(editInput, taskText);
+      
+          // Add an event listener to the input for when the user finishes editing
+          editInput.addEventListener('blur', () => {
+            // Update the task text with the edited content
+            taskText.textContent = editInput.value;
+        
+            // Remove the input element and show the updated task text
+            todoContainer.replaceChild(taskText, editInput);
+        });
+      
+        // Focus the input element to start editing
+        editInput.focus();
+      } )
+      })
   }
   
-  document.querySelector('.js-add-todo-button')
-    .addEventListener('click', () => {
+    document.querySelector('.js-add-todo-button').addEventListener('click', () => {
       addTodo();
     });
   
@@ -56,6 +80,5 @@ const todoList = [{
     });
   
     inputElement.value = '';
-  
     renderTodoList();
   }
